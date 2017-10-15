8134aef3fe63c44ef5b4af9d9d613154cdffdef4
walter-cc <imwalter8@gmail.com>
2017-10-15 11:01:10 +0800

    add size of pointer print

--- ch2_memory_pointer/ex_memory.c
+++ ch2_memory_pointer/ex_memory.c
@@ -5,7 +5,13 @@ int y = 1; // global variable is stored in the "global" area of memory
 int main(void){
 
 	int x = 4; //local variable is stored in the "stack" area of memory
+	int *p;
+
 	printf("x is stored in %p\n",&x);
+	printf("the size of x is %ld\n",sizeof(x));
+	//in 64-bit os, the size of pointer is 8, the size is 6 in 32-bit os
+	printf("the size of pointer for int is %ld\n",sizeof(p));	
+
 	return 0;
 }
 
