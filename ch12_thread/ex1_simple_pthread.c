cd7be5f75bb2cd794cc67ccd43abcffdf6ba3014
walter-cc <imwalter8@gmail.com>
2020-08-19 04:54:19 +0800

    modify code

--- ch12_thread/ex1_simple_pthread.c
+++ ch12_thread/ex1_simple_pthread.c
@@ -1,26 +1,47 @@
-/*===============================================================
-# compile method
-1) gen encrypt.o : gcc encrypt.c -c
-2) gen checksum.o : gcc checksum.c -c
-3) gen execute file "test" : gcc main.c checksum.o encrypt.o -o test
+/*===============================
+# 編譯和執行步驟 :
+1. gcc -c hello.c                                   // 產生目標檔(object file) hello.o
+2. gcc hello.o -o hello / gcc -o hello hello.o      // 產生執行檔(execute file) hello
+3. ./hello                                          // 執行 execute file "hello"
+or
+1. gcc hello.c -o hello
+2. ./hello
 
-# 此程式編譯和執行步驟 :
-gcc ex1_simple_pthread.c -lpthread -o main
-./main
+# 此範例執行步驟
+$ gcc ex1_simple_pthread.c -lpthread -o main    // 使用 gcc 編譯時，要加上 -lpthread 參數
+$ ./main
+
 ===============================
 # 此範例可以看到 :
+
 最簡易的POSIX thread程式庫(pthread)
+
 ===============================
-# 結果顯示 :
-cc@ch12_thread$./main
+# 執行結果 :
+
+cc@ch12_thread$gcc ex1_simple_pthread.c -lpthread -o main
+cc@ch12_thread$./main 
 Does too
 Does not
+Does too
+Does not
+Does too
 Does not
 Does too
 Does not
 Does too
-...
-===============================================================*/
+Does not
+
+
+===============================
+# 參考文件 :
+
+深入淺出 C，David Griffiths、Dawn Griffiths，O'Reilly
+Head First C: A Brain-Friendly Guide
+David Griffiths、Dawn Griffiths
+
+===============================*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -34,27 +55,29 @@ void error(char *msg)
     exit(1);
 }
 
-// 執行緒A
-void* does_not(void *a) // 執行緒函式必須具有(void*)回傳型別
-{
+// 執行緒A，執行緒函式必須具有(void*)回傳型別
+void* does_not(void *a) {
+
     int i = 0;
-    for(i = 0; i < 5; i++)
-    {
+
+    for(i = 0; i < 5; i++) {
         sleep(1);
         puts("Does not");
     }
+
     return NULL;        // 沒有東西要回傳，就使用NULL吧。
 }
 
-// 執行緒B
-void* does_too(void *a) // 執行緒函式必須具有(void*)回傳型別
-{
+// 執行緒B，執行緒函式必須具有(void*)回傳型別
+void* does_too(void *a) {
+
     int i = 0;
-    for(i = 0; i < 5; i++)
-    {
+
+    for(i = 0; i < 5; i++) {
         sleep(1);
         puts("Does too");
     }
+
     return NULL;        // 沒有東西要回傳，就使用NULL吧。
 }
 
@@ -65,13 +88,17 @@ int main(void){
 
     if( pthread_create(&t0, NULL, does_not, NULL) == -1 )   // does_not 為執行緒要執行的函式名稱
         error("Can't create thead t0");
+
     if( pthread_create(&t1, NULL, does_too, NULL) == -1 )   // does_too 為執行緒要執行的函式名稱
         error("Can't create thead t1");
 
+
     void* result;   // 從每個函式回傳的void指標將被存放在此。
+
 
     if( pthread_join(t0, &result) == -1 )   // pthread_join()函式等待執行緒結束
         error("Can't join thead t0");
+
     if( pthread_join(t1, &result) == -1 )   // pthread_join()函式等待執行緒結束
         error("Can't join thead t1");
 
--- ch12_thread/ex2_simple_pthread_return_value.c
+++ ch12_thread/ex2_simple_pthread_return_value.c
@@ -1,17 +1,25 @@
-/*===============================================================
-# compile method
-1) gen encrypt.o : gcc encrypt.c -c
-2) gen checksum.o : gcc checksum.c -c
-3) gen execute file "test" : gcc main.c checksum.o encrypt.o -o test
+/*===============================
+# 編譯和執行步驟 :
+1. gcc -c hello.c                                   // 產生目標檔(object file) hello.o
+2. gcc hello.o -o hello / gcc -o hello hello.o      // 產生執行檔(execute file) hello
+3. ./hello                                          // 執行 execute file "hello"
+or
+1. gcc hello.c -o hello
+2. ./hello
 
-# 此程式編譯和執行步驟 :
-gcc ex2_simple_pthread_return_value.c -lpthread -o main
-./main
+# 此範例執行步驟
+$ gcc ex2_simple_pthread_return_value.c -lpthread -o main    // 使用 gcc 編譯時，要加上 -lpthread 參數
+$ ./main
+
 ===============================
 # 此範例可以看到 :
+
 當我們想要傳遞及回傳整數值給執行緒，技巧之一是使用「long」值，「long」可以被儲存在void指標裡，因為他們具有相同的尺寸。
+
 ===============================
-# 結果顯示 :
+# 執行結果 :
+
+cc@ch12_thread$gcc ex2_simple_pthread_return_value.c -lpthread -o main
 cc@ch12_thread$./main
 Thread number 2
 Thread number 1
@@ -19,7 +27,16 @@ Thread number 0
 Thread 0 returned 1
 Thread 1 returned 2
 Thread 2 returned 3
-===============================================================*/
+
+===============================
+# 參考文件 :
+
+深入淺出 C，David Griffiths、Dawn Griffiths，O'Reilly
+Head First C: A Brain-Friendly Guide
+David Griffiths、Dawn Griffiths
+
+===============================*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -31,7 +48,9 @@ Thread 2 returned 3
 void* do_stuff(void* param) // (void* param) : 執行緒函式能夠接受單一的void指標參數
 {
     long thread_no = (long)param;       //將param轉換回long
+
     printf("Thread number %ld\n", thread_no);
+
     return (void*)(thread_no +1);       // 當它被回傳時，把它強制轉型回void指標。
 }
 
@@ -44,9 +63,11 @@ int main(void){
         pthread_create(&threads[t], NULL, do_stuff, (void*)t); // (void*)t : 先將long變數t的值換成void指標，再傳到do_stuff裡的參數(void* param)。
 
     void* result;   // 從每個函式回傳的void指標將被存放在此。
-    for(t = 0; t < 3; t++)
-    {
+
+    for(t = 0; t < 3; t++) {
+
         pthread_join(threads[t], &result);   // pthread_join()函式等待執行緒結束，reulst 存放 return (void*)(thread_no +1)
+
         printf("Thread %ld returned %ld\n", t, (long)result); // 使用之前，先將回傳值轉換成long。
     }
 
